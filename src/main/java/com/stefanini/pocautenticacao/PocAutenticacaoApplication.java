a747439c14f893ac3b883661e5d55202cded1c2f
wmcosta <wmcosta@latam.stefanini.com>
2018-09-13 13:12:28 -0300

    Arquivos basicos de configuração

---
+++ src/main/java/com/stefanini/pocautenticacao/PocAutenticacaoApplication.java
@@ -0,0 +1,12 @@
+package com.stefanini.pocautenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocAutenticacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocAutenticacaoApplication.class, args);
+	}
+}
